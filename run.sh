4bd86f9a9dd45d500a4e75c1fbb4f613efdc2575
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-04 09:12:26 -0400

    initial

--- run.sh
+++
@@ -1,3 +0,0 @@
-podman login -u iamapikey -p $ENTITLEMENT_KEY
-podman pull icr.io/zoscp/oeftools:latest
-podman run -it --rm --uts host --annotation zoscp.ibm.com/override-networking=host 44e13a0602c9
